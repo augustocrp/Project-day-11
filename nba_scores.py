385de45ce651abb42c7b2d7f8aaec18c1dd96309
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-27 23:26:01 -0300

    Update nba_scores.py

--- nba_scores.py
+++ nba_scores.py
@@ -41,7 +41,7 @@ def get_teams_stats():
 
 while True:
     print("#######################\n")
-    print("SEJA BEM VINDO!!! DADOS NBA\n")
+    print("SEJA BEM VINDO! DADOS NBA\n")
     print("1 - Ver Jogos\n")
     print("2 - Ver Times por PPG\n")
     user_choice = input("Opção: ")
